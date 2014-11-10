e9615077d9e1a38c61c795396b5eee9d8fc92080
Francis John <francis.john@colorado.edu>
2014-11-09 16:51:31 -0800

    Puts current user

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,4 +1,7 @@
 
 def current_user
-  @current_user ||= User.find_by(:fb_user_id => session[:user_id]) if session[:user_id]
+	puts "*"*50
+	puts "Session User ID: #{session[:user_id]}"
+	puts "*"*50
+	@current_user ||= User.find_by(:fb_user_id => session[:user_id]) if session[:user_id]
 end
