ae5ef5cd47faf0798750caf9c4728ad7b8f907e4
Francis John <francis.john@colorado.edu>
2014-11-09 20:45:11 -0800

    add console logging

--- public/js/view/view.js
+++ public/js/view/view.js
@@ -3,6 +3,7 @@ var view = (function(){
   var counter = 0;
 
   var addNewSplash = function(data){
+    console.log(data)
     var timeCreatedAt = Date.parse(data.created_at);
     var timeNow = new Date();
     var timeDifference = timeNow - timeCreatedAt;
