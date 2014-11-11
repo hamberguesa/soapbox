b68f99f41d0626341b1171deea96b7cd25b6c4a4
samuraijacques <choongmarn@gmail.com>
2014-11-11 11:28:31 -0800

    found out where all the times are being set the same

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,18 +4,19 @@ def current_user
 end
 
 def time_ago
+  # this is where all the times are being set to the last splash time
   hours = ((Time.now - Splash.last.created_at)/3600).floor
   minutes = ((Time.now - Splash.last.created_at)/60).to_i
   if minutes == 0
     minutes += 1
-  else 
+  else
     minutes = minutes
   end
 
   if hours > 0
     @time_created = "#{hours} hour #{minutes} minutes ago"
   end
-    if minutes < 2 
+    if minutes < 2
       @time_created = "#{minutes} minute ago"
     else
       @time_created = "#{minutes} minutes ago"
